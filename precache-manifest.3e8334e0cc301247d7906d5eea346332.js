2d4d60b580b510278786a5e09062011c54bb2516
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-03-03 16:14:21 +0530

    Updates

---
+++ precache-manifest.3e8334e0cc301247d7906d5eea346332.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7b20b408fae54d6af343c8884cf6be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a576c689f12be11b08",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "3b2c48525b21fca07346",
+    "url": "/static/js/2.0b538cd4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0b538cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a576c689f12be11b08",
+    "url": "/static/js/main.af465f0a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2b3fff2eac61af13ba170f446fde53a6",
+    "url": "/static/media/resume.2b3fff2e.pdf"
+  },
+  {
+    "revision": "fb0c7a555fe5e1809f67d64213c86475",
+    "url": "/static/media/saipic.fb0c7a55.jpg"
+  }
+]);
